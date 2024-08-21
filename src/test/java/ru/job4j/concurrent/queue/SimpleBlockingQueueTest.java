e35323d26ba30139bbb837426fba76631a541801
Kiril-k23 <kirillkleyn@gmail.com>
2024-08-21 21:57:16 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/concurrent/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/queue/SimpleBlockingQueueTest.java
@@ -1,40 +1,47 @@
 package ru.job4j.concurrent.queue;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-
-class SimpleBlockingQueueTest {
+public class SimpleBlockingQueueTest {
     @Test
-    public void queueTest() throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
-        List<Integer> list = new ArrayList<>();
-        Thread producer = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                try {
-                    queue.offer(i);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+    public void QueueTest() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(40);
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 3).forEach(
+                            value -> {
+                                try {
+                                    queue.offer(value);
+                                } catch (InterruptedException e) {
+                                    Thread.currentThread().interrupt();
+                                }
+                            }
+                    );
                 }
-            }
-        });
-        Thread consumer = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                try {
-                    list.add(queue.poll());
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    try {
+                        while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                            buffer.add(queue.poll());
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
-            }
-        });
+        );
         producer.start();
         consumer.start();
         producer.join();
+        consumer.interrupt();
         consumer.join();
-        assertThat(list).containsExactly(0, 1, 2, 3, 4);
+        assertThat(buffer).containsExactly(0, 1, 2);
     }
-
 }
